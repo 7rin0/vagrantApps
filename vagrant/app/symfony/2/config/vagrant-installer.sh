93a6e4642079c65f8a93b5d307a862237a6cee21
Luis SEVERINO <lfs.severino@gmail.com>
2016-02-14 00:43:41 +0100

    fix script to sf2

--- vagrant/app/symfony/2/config/vagrant-installer.sh
+++ vagrant/app/symfony/2/config/vagrant-installer.sh
@@ -7,7 +7,7 @@ cd /vagrant/app/symfony/2 && sudo rm -rf current && symfony new current 2.8
 sudo mysql -u root -proot -h localhost -e'create database symfony_2'
 sudo chmod -R 777 /vagrant/app/symfony/2/current/app/cache
 sudo chmod -R 777 /vagrant/app/symfony/2/current/app/logs
-sudo sed -i "s/'127.0.0.1'/'127.0.0.1', '192.168.75.1'/g" /vagrant/app/symfony/3/current/web/app_dev.php
+sudo sed -i "s/'127.0.0.1'/'127.0.0.1', '192.168.75.1'/g" /vagrant/app/symfony/2/current/web/app_dev.php
 
 # Set params
 sudo cp -f /vagrant/app/symfony/2/config/symfony_2.conf /etc/apache2/sites-available/symfony_2.conf
