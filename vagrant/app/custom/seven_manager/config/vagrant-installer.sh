d364af2d1537a9c1d201130a8f3d5c68a2c921f0
Luis SEVERINO <lfs.severino@gmail.com>
2016-02-19 00:33:51 +0100

    add host do seven-manager project

--- vagrant/app/custom/seven_manager/config/vagrant-installer.sh
+++ vagrant/app/custom/seven_manager/config/vagrant-installer.sh
@@ -7,19 +7,17 @@ mv current releases/seven_manager_$(date +%F-%T)
 git clone https://github.com/7rin0/SevenManager.git current
 cd current
 sudo DATABASE_NAME=seven_manager DATABASE_USER=root DATABASE_PASSWORD="root" COMPOSER_PROCESS_TIMEOUT=900 composer install -n
-#sudo cp -f /vagrant/app/custom/seven_manager/config/parameters.yml /vagrant/app/custom/seven_manager/current/app/config/parameters.yml
-sudo mysql -u root -proot -h localhost -e'create database seven_manager'
-#sudo chmod -R 777 /vagrant/app/custom/seven_manager/current/app/cache
-#sudo chmod -R 777 /vagrant/app/custom/seven_manager/current/app/logs
+sudo mysql -u root -proot -h localhost -e'create database seven_manager' --if-not-exists
 sudo sed -i "s/'127.0.0.1'/'127.0.0.1', '192.168.75.1'/g" /vagrant/app/custom/seven_manager/current/web/app_dev.php
+sudo cp -f /vagrant/app/custom/seven_manager/config/parameters.yml /vagrant/app/custom/seven_manager/current/app/config/parameters.yml
 
-# Install seven_manager's project ready to run
-cd /vagrant/app/custom/seven_manager/current
-sudo COMPOSER_PROCESS_TIMEOUT=600 composer install -n
-sudo COMPOSER_PROCESS_TIMEOUT=600 composer update -n
-php app/console doctrine:database:create --if-not-exists -n
-php app/console doctrine:schema:create
-php app/console fos:user:create --super-admin admin admin@domain.com admin
+# Install project
+php app/console doctrine:phpcr:init:dbal --force
+php app/console doctrine:phpcr:repository:init
+php app/console doctrine:phpcr:fixtures:load -n
+php app/console fos:user:create admin admin@admin.com admin --super-admin
+php app/console assets:install
+php app/console assetic:dump
 
 # Set params
 sudo cp -f /vagrant/app/custom/seven_manager/config/seven_manager.conf /etc/apache2/sites-available/seven_manager.conf
