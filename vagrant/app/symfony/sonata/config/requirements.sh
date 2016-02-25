feb0efc966d9bc46d3ae316dd4d6b1116a986060
Luis SEVERINO <lfs.severino@gmail.com>
2016-02-25 02:47:51 +0100

    add sonata install

--- vagrant/app/symfony/sonata/config/requirements.sh
+++ vagrant/app/symfony/sonata/config/requirements.sh
@@ -6,9 +6,12 @@ mkdir releases
 mv current releases/sonata_$(date +%F-%T)
 
 # Create current directory and install project and their vendors
-git clone https://github.com/7rin0/sandbox.git current
+cd ~ && git clone https://github.com/7rin0/sandbox.git current
+sudo cp -f /vagrant/app/symfony/sonata/config/parameters.yml /vagrant/app/symfony/sonata/current/app/config/parameters.yml
 cd current
-composer install --prefer-source --no-interaction
+composer install --prefer-source -n --profile
+composer install --n --profile
+cd ../ && sudo mv current /vagrant/app/symfony/sonata/
 
 # Set permissions
 sudo chmod -R 777 /vagrant/app/symfony/sonata/current/app/cache
@@ -16,17 +19,16 @@ sudo chmod -R 777 /vagrant/app/symfony/sonata/current/app/logs
 sudo chown -R $(whoami):www-data *
 
 # Set configs, Create database schema and load data
-sudo cp -f /vagrant/app/symfony/sonata/config/parameters.yml /vagrant/app/symfony/sonata/current/app/config/parameters.yml
 sudo mysql -u root -proot -h localhost -e'create database sonata'
 cd /vagrant/app/symfony/sonata/current
 php app/console doctrine:schema:create
-php app/console doctrine:fixtures:load -y
+php app/console doctrine:schema:update --force
+php app/console doctrine:fixtures:load -n
+php app/console assets:install
 php app/console fos:user:create --super-admin admin admin@domain.com admin
 sudo sed -i "s/'127.0.0.1'/'127.0.0.1', '192.168.75.1'/g" /vagrant/app/symfony/sonata/current/web/app_dev.php
 
 # Add host and restart apache
 sudo cp -f /vagrant/app/symfony/sonata/config/sonata.conf /etc/apache2/sites-available/sonata.conf
 sudo a2ensite sonata
-
-# Setup a Sonata/Symfony CMF project
-#isudo DATABASE_NAME=sonata DATABASE_USER=root DATABASE_PASSWORD="root" COMPOSER_PROCESS_TIMEOUT=600 composer create-project sonata-project/sandbox:2.3.x-dev current -n
+sudo service apache2 restart
--- vagrant/install.sh
+++ vagrant/install.sh
@@ -59,13 +59,13 @@ sudo cat /vagrant/config/hosts/hosts >> /etc/hosts
 . /vagrant/app/symfony/3/config/requirements.sh
 
 # Comment next line to avoid last Sonata installation
-#. /vagrant/app/symfony/sonata/config/requirements.sh
+. /vagrant/app/symfony/sonata/config/requirements.sh
 
 # Comment next line to avoid last Wordpress 4 installation
 . /vagrant/app/wordpress/4/config/requirements.sh
 
 # Comment next line to avoid last Seven Manager (custom project) installation
-#. /vagrant/app/custom/seven_manager/config/requirements.sh
+. /vagrant/app/custom/seven_manager/config/requirements.sh
 
 # Restart services
 sudo /etc/init.d/apache2 restart -y
