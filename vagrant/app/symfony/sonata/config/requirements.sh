e8dff906cfb9949332bcebe4c0fabc0d7fc0fcc0
Luis SEVERINO <lfs.severino@gmail.com>
2016-03-01 01:30:11 +0100

    update sonata commands

--- vagrant/app/symfony/sonata/config/requirements.sh
+++ vagrant/app/symfony/sonata/config/requirements.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-# Move old source to releases by date
-cd /vagrant/app/symfony/sonata
-mkdir releases
-mv current releases/sonata_$(date +%F-%T)
+# Prepare database
+sudo mysql -u root -proot -h localhost -e'create database sonata'
 
 # Create current directory and install project and their vendors
-cd ~ && git clone https://github.com/7rin0/sandbox.git current
-sudo cp -f /vagrant/app/symfony/sonata/config/parameters.yml /vagrant/app/symfony/sonata/current/app/config/parameters.yml
+cd /home/vagrant && rm -rf current
+git clone https://github.com/7rin0/sandbox.git current
+sudo cp -f /vagrant/app/symfony/sonata/config/parameters.yml ~/current/app/config/parameters.yml
+sudo chmod 777 ~/current/app/config/parameters.yml
 cd current
 composer install --prefer-source -n --profile
-composer install --n --profile
-cd ../ && sudo mv current /vagrant/app/symfony/sonata/
+composer install -n --profile
+
+# Move old source to releases by date
+mkdir -p /vagrant/app/symfony/sonata/releases
+mv /vagrant/app/symfony/sonata/current /vagrant/app/symfony/sonata/releases/sonata_$(date +%F-%T)
+sudo mv /home/vagrant/current /vagrant/app/symfony/sonata/
 
 # Set permissions
+cd /vagrant/app/symfony/sonata/current
 sudo chmod -R 777 /vagrant/app/symfony/sonata/current/app/cache
 sudo chmod -R 777 /vagrant/app/symfony/sonata/current/app/logs
 sudo chown -R $(whoami):www-data *
 
 # Set configs, Create database schema and load data
-sudo mysql -u root -proot -h localhost -e'create database sonata'
-cd /vagrant/app/symfony/sonata/current
 php app/console doctrine:schema:create
 php app/console doctrine:schema:update --force
-php app/console doctrine:fixtures:load -n
+php app/console doctrine:fixtures:load --process-isolation --no-ansi --no-debug -n
 php app/console assets:install
 php app/console fos:user:create --super-admin admin admin@domain.com admin
 sudo sed -i "s/'127.0.0.1'/'127.0.0.1', '192.168.75.1'/g" /vagrant/app/symfony/sonata/current/web/app_dev.php
