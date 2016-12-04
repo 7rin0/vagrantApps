9579aee614990903d1ed08a7b5068927e1ed0ecb
Luis SEVERINO <lfs.severino@gmail.com>
2016-12-04 01:54:06 +0100

    add php7 and update repositories

--- .vgfiles/ubuntu/xenial/hashicorp/install.sh
+++ .vgfiles/ubuntu/xenial/hashicorp/install.sh
@@ -1,45 +1,22 @@
 #!/bin/bash
 
-# No interactive settings
-sudo cp -f /vagrant/config/grub /etc/default/grub
-sudo update-grub
+# Later this script will be split into parts, clusters and for some services a dedicated container is also being prepared and tested ...
 
-# Default variables to no interaction installations
-echo mysql-server mysql-server/root_password select root | sudo debconf-set-selections
-echo mysql-server mysql-server/root_password_again select root | sudo debconf-set-selections
-sudo sed -i '/tty/!s/mesg n/tty -s \\&\\& mesg n/' /root/.profile
+# Set global variables
+export DEBIAN_FRONTEND=noninteractive
+export COMPOSER_ALLOW_SUPERUSER=1
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
-# Update packages
-sudo DEBIAN_FRONTEND=noninteractive apt-get -f install -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-
-# Install LAMP
-sudo DEBIAN_FRONTEND=noninteractive apt-get install apache2 -y
-sudo sed -i -e '1 i\ ServerName localhost ' /etc/apache2/apache2.conf
-sudo DEBIAN_FRONTEND=noninteractive apt-get install mysql-server -y
-sudo apt-get install php5 -y
-sudo apt-get install php5 libapache2-mod-php5 -y
-sudo apt-get install php5-dev -y
-sudo apt-get install php5-mcrypt -y
-sudo apt-get install php5-curl -y
-sudo apt-get install php5-gd -y
-sudo apt-get install php5-cli -y
-sudo apt-get install php5-mysql -y
-sudo apt-get install pdo-mysql -y
-sudo a2enmod rewrite
-sudo service apache2 restart -y
-
-# Prepare Environment
-sudo apt-get install zip -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get install git -y
-sudo curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/bin/composer
-sudo composer config -g github-oauth.github.com f0502ecd3d7c8e7e47223616c177b869180a3e05
-
-# Install cache accelerators and server services
-sudo apt-get install php5-memcache memcached php-pear -y
-sudo pecl install memcache
-echo "extension=memcache.so" | sudo tee /etc/php5/apache2/conf.d/memcache.ini
-
-# Restart services
-sudo /etc/init.d/apache2 restart -y
+# http://www.cyberciti.biz/faq/ubuntu-linux-14-04-install-php7-using-apt-get-command/
+# https://github.com/oerdnj/deb.sury.org/issues/56
+# Setup simple lamp environment.
+# Install PHP.
+set -gx  LC_ALL en_US.UTF-8
+sudo apt-get update && apt-get install -y language-pack-en-base
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository --yes ppa:ondrej/php
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install php7.0 php7.0-cli php7.0-fpm php7.0-gd php7.0-json php7.0-mysql php7.0-readline  php7.0-bcmath  php7.0-mbstring  php7.0-soap php7.0-xml php7.0-zip -y
